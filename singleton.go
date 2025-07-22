487d2da4b3c0d51d05e7ff40c72cb6b71aa960c6
kyoshee <dsuchkov511@gmail.com>
2025-07-22 17:19:20 +0300

    add singleton.go

---
+++ singleton.go
@@ -0,0 +1,26 @@
+package singleton
+
+import (
+	"sync"
+)
+
+type Singleton[T any] struct {
+	once  sync.Once
+	f     func() *T
+	value *T
+}
+
+func New[T any](f func() *T) *Singleton[T] {
+	return &Singleton[T]{
+		once:  sync.Once{},
+		value: nil,
+	}
+}
+
+func (s *Singleton[T]) Get() *T {
+	s.once.Do(func() {
+		s.value = s.f()
+	},
+	)
+	return s.value
+}
